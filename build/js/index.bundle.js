f180ca6bf12fbfb7c422bd53bd6a01d0c7f7326f
ViacheslavAnisimov <forvsn@gmail.com>
2024-10-09 12:09:25 +0700

    feat: add adaptive

--- build/js/index.bundle.js
+++ build/js/index.bundle.js
@@ -3,17 +3,17 @@
 
 function mobileMenu() {
   const mobileMenuButton = document.querySelector('.mobile-menu-btn');
-  const mobileMenu = document.querySelector('.mobile-menu');
+  const mobileMenu = document.querySelector('.nav-menu');
 
   mobileMenuButton.addEventListener('click', () => {
     mobileMenuButton.classList.toggle('menu-active');
-    mobileMenu.classList.toggle('mobile-menu-open');
+    mobileMenu.classList.toggle('nav-menu-open');
     document.body.classList.toggle('lock');
   })
 
   document.addEventListener('click', (event) => {
     if (!mobileMenu.contains(event.target) && event.target !== mobileMenuButton || event.target.classList.contains('nav-link')) {
-      mobileMenu.classList.remove('mobile-menu-open');
+      mobileMenu.classList.remove('nav-menu-open');
       mobileMenuButton.classList.remove('menu-active');
       document.body.classList.remove('lock');
     }
